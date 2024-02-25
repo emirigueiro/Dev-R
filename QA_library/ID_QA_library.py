15c550fe457f56581e246d21f5b1d4e64fe20d6c
EmiRigueiro <rigueiroemiliano@gmail.com>
2024-02-25 13:05:49 -0300

    New id_quality

--- QA_library/ID_QA_library.py
+++ QA_library/ID_QA_library.py
@@ -3,8 +3,8 @@ import numpy as np
 import re
 import Common_fuctions_QA_library 
 
-
 #Identifica si el nombre de la columna contiene la expresión regular "id/ID"
+
 def id_list(x):
     column_list = x.columns
     patron = re.compile(r"id", re.IGNORECASE)
@@ -18,7 +18,6 @@ def id_list(x):
             
     return(resultados)
 
-#Funcion para calcular la probabilidad de que se trate de un campo ID valido.
 def id_probability(x):
    #Tranforma el True por 1 y False por 0, sobre el return de la funciòn id_list:
    id_re = [0 if x == True else 1 for x in (id_list(x))]
@@ -34,11 +33,27 @@ def id_probability(x):
          unique_and_distincts.append(1)
       elif x1 == x2:
          unique_and_distincts.append(0)
+
    #Utiliza la funciòn empty_list para identifica aquellas columnas que cuentan con valores vacios y reemplaza Ture x 1 y False x 0:
    empty_rows = [1 if x > 0 else 0 for x in (Common_fuctions_QA_library.empty_list(x))]
 
    #Utiliza la funciòn  null_list para identifica aquellas columnas que cuentan con valores nulos y reemplaza Ture x 1 y False x 0:
    null_rows = [1 if x > 0 else 0 for x in (Common_fuctions_QA_library.null_list(x))]
+
+   #Creacion variable % de unicos:
+   percent_unique_1 = []
+   percent_unique_2 = [] 
+   [percent_unique_1.append((x2 / x1)*100) for x1, x2 in zip(count_rows, count_distinct_rows)]
+   
+   for x in percent_unique_1:
+       if x <= 100.0 and x >= 90.1:
+           percent_unique_2.append(0)
+       elif x <= 90.0 and x >= 50.1:
+           percent_unique_2.append(1)
+       elif x <= 50.0 and x >= 20.1:
+           percent_unique_2.append(2)
+       elif x <= 20.0:
+           percent_unique_2.append(3)
 
    #Sumarizaciòn:
    final = pd.DataFrame()
@@ -46,10 +61,12 @@ def id_probability(x):
    final['unique_and_distincts'] = unique_and_distincts
    final['empty_rows'] = empty_rows
    final['null_rows'] = null_rows
+   final['percent_unique'] = percent_unique_2
   
    final_2 = []
-   final_2 = final['id_re'] + final['unique_and_distincts'] + final['empty_rows'] + final['null_rows']
-
+   final_2 = final['id_re'] + final['unique_and_distincts'] + final['empty_rows'] + final['null_rows'] + final['percent_unique']
+   
+   #Construcciòn del calcuo de probabilidad
    id_quality = []
    for x in final_2:
        if x == 0:
@@ -58,9 +75,7 @@ def id_probability(x):
            id_quality.append('75%')
        elif x == 2:
            id_quality.append('50%')
-       elif x == 3:
-           id_quality.append('25%')
-       elif x == 4:
-           id_quality.append('')        
+       elif x >= 3:
+           id_quality.append('0%')
 
    return id_quality
